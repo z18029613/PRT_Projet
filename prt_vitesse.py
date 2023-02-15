fe3dc15e4c37e9c7e62d81cf1b2bfebbb18513d5
Usakumo <123679109+Usakumo@users.noreply.github.com>
2023-02-15 09:17:09 +0100

    dernière maj 15/02
    pour tester si le code sur le git et celui là sont les mêmes

--- prt_vitesse.py
+++ prt_vitesse.py
@@ -6,30 +6,21 @@ Lorenzo ROSADO
 
 #Libraries importées
 import numpy as np
-from numpy.fft import fft,ifft, fftshift
 import matplotlib.pyplot as plt
 import scipy as sc
 import scipy.io.wavfile as wav
-
+import io
 
 
 #Bloc d'importation du fichier 
-uploaded = "C:/Users/easyr/Desktop/Son_a.wav"
+uploaded = "C:/Users/Domi/Desktop/AAA.wav"
 #Variable contenant le son : 
-    
 Son_AAA = wav.read(uploaded)
-
 Son_freq = Son_AAA[0]
-Son_array = Son_AAA[1][:,0] # <!> il serait de taille N x 2 car il y a deux canaux audios, oreille gauche et oreille droite.
-                            # ici nous n'en utilisons qu'une seule. A l'avenir son mono serait mieux
-
-Son_freq = int(Son_freq/2) # fe/2
-fe = Son_freq
-Son_fft = fft(Son_array) / 2
-Son_comprime = np.abs ( Son_fft )
-
+Son_array = Son_AAA[1] # <!> il est de taille N x 2 car il y a deux canaux audios, oreille gauche et oreille droite.
+Son_array_destruct=Son_array/2
 #écrire le fichier wav et le télécharger avec méthode 1
-test = wav.write("test.wav", Son_freq, Son_comprime )
+test = wav.write("test.wav", Son_freq*2, Son_array_destruct )
 #Avec cette méthode le son sort presque détruit, on ne reconnait même pas le signal d'entrée
 
 Son_AAA_fft = np.fft.fft(Son_array, norm="ortho")
@@ -38,12 +29,13 @@ Son_AAA_selfconvol  = np.abs(np.fft.ifft(np.abs(Son_AAA_fft), norm="ortho"))
 Son_AAA_selfconvol=Son_AAA_selfconvol/Son_AAA_selfconvol[0]
 
 
-N = int(len(Son_array /2))
-x = np.linspace(-fe,fe,N)
-plt.plot(x,np.abs(np.fft.fftshift(np.fft.fft(Son_array))) , 'b',label = "son de base")
-plt.plot(x,np.abs(np.fft.fftshift(np.fft.fft(Son_comprime))) ,'r',label = "son de comprimé")
-plt.legend()
-plt.title("Fréquence d'un son produit par la prononciation d'un A et sa compression.")
+N = int(89019/2)
+x = np.arange(-N,N+1)
+plt.plot(x,np.log(np.abs(np.fft.fftshift(np.fft.fft(Son_array[:,0])))) , 'b')
+plt.plot(x,np.log(np.abs(np.fft.fftshift(np.fft.fft(Son_array_destruct[:,0])))) ,'r')
+
 plt.xlabel("frequence")
 plt.ylabel("log (|S(f)|")
+
+
 plt.show()
