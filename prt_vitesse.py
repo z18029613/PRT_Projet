e17caec70505a8bb856688a4faeac672961c272d
Usakumo <123679109+Usakumo@users.noreply.github.com>
2023-02-15 22:44:02 +0100

    15/02 filtre
    tentative de filtre, paramètres erronés

--- prt_vitesse.py
+++ prt_vitesse.py
@@ -7,35 +7,87 @@ Lorenzo ROSADO
 #Libraries importées
 import numpy as np
 import matplotlib.pyplot as plt
-import scipy as sc
 import scipy.io.wavfile as wav
-import io
 
+
+ 
+plt.close(fig='all')
 
 #Bloc d'importation du fichier 
-uploaded = "C:/Users/Domi/Desktop/AAA.wav"
+uploaded = "Son_a.wav"
+
 #Variable contenant le son : 
 Son_AAA = wav.read(uploaded)
-Son_freq = Son_AAA[0]
-Son_array = Son_AAA[1] # <!> il est de taille N x 2 car il y a deux canaux audios, oreille gauche et oreille droite.
+Son_freq = Son_AAA[0]           # fréquence du fichier
+Son_array = Son_AAA[1]          # <!> il est de taille N x 2 car il y a deux canaux audios, oreille gauche et oreille droite.
 Son_array_destruct=Son_array/2
+
 #écrire le fichier wav et le télécharger avec méthode 1
-test = wav.write("test.wav", Son_freq*2, Son_array_destruct )
+file_test = wav.write("test_compression.wav", Son_freq*2, Son_array_destruct )
 #Avec cette méthode le son sort presque détruit, on ne reconnait même pas le signal d'entrée
 
+
+
 Son_AAA_fft = np.fft.fft(Son_array, norm="ortho")
-Son_AAA_selfconvol  = np.abs(np.fft.ifft(np.abs(Son_AAA_fft), norm="ortho"))
-
-Son_AAA_selfconvol=Son_AAA_selfconvol/Son_AAA_selfconvol[0]
-
-
-N = int(89019/2)
+N = int(len(Son_AAA_fft)/2)
 x = np.arange(-N,N+1)
-plt.plot(x,np.log(np.abs(np.fft.fftshift(np.fft.fft(Son_array[:,0])))) , 'b')
-plt.plot(x,np.log(np.abs(np.fft.fftshift(np.fft.fft(Son_array_destruct[:,0])))) ,'r')
 
+plt.plot(x,np.abs(np.fft.fftshift(np.fft.fft(Son_array[:,0]))) , 'b',label="TF du son 'a'")
+plt.plot(x,np.abs(np.fft.fftshift(np.fft.fft(Son_array_destruct[:,0]))) ,'r',label="TF du son après compression")
 plt.xlabel("frequence")
-plt.ylabel("log (|S(f)|")
+plt.ylabel("|S(f)|")
 
 
+#Filtre passe bas
+n = np.arange(0,len(Son_AAA_fft))
+n_temp = np.zeros ( np.shape(Son_array) , dtype="int16")
+n_temp[:,0],n_temp[:,1] = n , n 
+n = n_temp
+
+fe = Son_freq
+
+Freq = np.linspace(-fe, fe, len(n))
+F_temp = np.zeros ( np.shape(Son_array) , dtype="int16")
+F_temp[:,0],F_temp[:,1] = Freq, Freq
+Freq = F_temp
+
+h = np.zeros ( np.shape(Son_array))# , dtype="int16")
+freq_coupure = 400000
+std = freq_coupure
+coef = 1000
+
+h[:,0] = 1/np.sqrt(2 * np.pi * std**2)  * np.exp(- Freq[:,0]**2 / (2* std**2) ) * coef
+h[:,1] = 1/np.sqrt(2 * np.pi * std**2)  * np.exp(- Freq[:,1]**2 / (2* std**2) )  *coef
+
+
+
+H = h #np.fft.fft(h)
+
+Son_filtre = H * Son_AAA_fft 
+
+plt.plot(x,np.abs(np.fft.fftshift(np.fft.fft(Son_filtre[:,0]))) , 'g',label="TF filtré par H")
+plt.legend()
+plt.figure(1)
 plt.show()
+
+plt.figure(2)
+plt.plot(Freq ,  np.abs (    H ) , label = "|H|")
+plt.legend()
+plt.show()
+
+
+plt.figure(3)
+
+Son_AAA_postfiltre = np.array(  np.abs( np.fft.ifft (Son_filtre) ) , dtype = "int16")
+
+plt.plot(Son_AAA_postfiltre,label="son post filtre")
+plt.legend()
+plt.show()
+
+file_sonpostfiltre = wav.write("sonpostfiltre.wav", Son_freq , Son_AAA_postfiltre )
+
+
+
+
+
+
